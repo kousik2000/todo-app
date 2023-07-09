3757ee14f30eb2fa892efa0ac183d7fe88af304c
kousik2000 <kousik.ramachandruni@gmail.com>
2023-07-09 10:42:04 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ interface Todo {
   id: number;
   text: string;
   completed: boolean;
-  editMode: boolean;
 }
 
 const TodoApp: React.FC = () => {
@@ -32,7 +31,6 @@ const TodoApp: React.FC = () => {
       id: todos.length + 1,
       text: todoText,
       completed: false,
-      editMode: false,
     };
 
     setTodos([...todos, newTodo]);
@@ -50,33 +48,6 @@ const TodoApp: React.FC = () => {
         return {
           ...todo,
           completed: !todo.completed,
-        };
-      }
-      return todo;
-    });
-    setTodos(updatedTodos);
-  };
-
-  const toggleEditMode = (id: number) => {
-    const updatedTodos = todos.map(todo => {
-      if (todo.id === id) {
-        return {
-          ...todo,
-          editMode: !todo.editMode,
-        };
-      }
-      return todo;
-    });
-    setTodos(updatedTodos);
-  };
-
-  const updateTodoText = (id: number, newText: string) => {
-    const updatedTodos = todos.map(todo => {
-      if (todo.id === id) {
-        return {
-          ...todo,
-          text: newText,
-          editMode: false,
         };
       }
       return todo;
@@ -109,50 +80,24 @@ const TodoApp: React.FC = () => {
             key={todo.id}
             className='flex items-center justify-between mb-2 min-w-[340px] mt-5 border border-blue p-3 rounded'
           >
-            {todo.editMode ? (
+            <div className='flex items-center'>
               <input
-                type='text'
-                value={todo.text}
-                onChange={e => updateTodoText(todo.id, e.target.value)}
-                className='border border-gray-300 rounded p-2 mr-2'
+                type='checkbox'
+                checked={todo.completed}
+                onChange={() => toggleComplete(todo.id)}
+                className='mr-2'
               />
-            ) : (
-              <div className='flex items-center'>
-                <input
-                  type='checkbox'
-                  checked={todo.completed}
-                  onChange={() => toggleComplete(todo.id)}
-                  className='mr-2'
-                />
-                <span className={todo.completed ? 'line-through' : ''}>
-                  {todo.text}
-                </span>
-              </div>
-            )}
-
-            <div>
-              {todo.editMode ? (
-                <button
-                  className='bg-green-500 hover:bg-green-700 text-white py-1 px-2 rounded mr-1'
-                  onClick={() => toggleEditMode(todo.id)}
-                >
-                  Save
-                </button>
-              ) : (
-                <button
-                  className='bg-yellow-500 hover:bg-yellow-700 text-white py-1 px-2 rounded mr-2'
-                  onClick={() => toggleEditMode(todo.id)}
-                >
-                  Edit
-                </button>
-              )}
-              <button
-                onClick={() => deleteTodo(todo.id)}
-                className='bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded'
-              >
-                Delete
-              </button>
+              <span className={todo.completed ? 'line-through' : ''}>
+                {todo.text}
+              </span>
             </div>
+
+            <button
+              onClick={() => deleteTodo(todo.id)}
+              className='bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded'
+            >
+              Delete
+            </button>
           </li>
         ))}
       </ul>
